8d5c291d5af9ed83468a61f174c6a4603081633e
Emilio Nicoli <emilionicoli@gmail.com>
2024-08-18 20:15:08 -0600

    not sure pre deploy added pnpm static config

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import Icons from 'unplugin-icons/vite'
 import { FileSystemIconLoader } from 'unplugin-icons/loaders'
